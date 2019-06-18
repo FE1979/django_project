452e0b5bec814459069fec3d26f0a24c9fab283d
FE1979 <ronin911@gmail.com>
2019-06-18 10:36:19 +0300

    First app start

--- students/views.py
+++ students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def students_list(request):
+    return HttpResponse("""
+                        <h1>World</h1>
+                        <h2>Hello</h2>
+                       """
+                       )
