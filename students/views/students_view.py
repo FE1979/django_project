4adae3ec50726a981474a413bb7b21b1d5c6f13e
FE1979 <ronin911@gmail.com>
2019-08-14 08:40:27 +0300

    fine visual edit form using crispy forms

--- students/views/students_view.py
+++ students/views/students_view.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.forms import ModelForm
 from django.views.generic import UpdateView
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.bootstrap import FormActions
 
 from ..models import Student, Group
 from datetime import datetime
@@ -155,12 +160,42 @@ def students_add(request):
         return render(request, 'students/students_add.html',
                   {'groups': Group.objects.all().order_by('title')})
 
+class StudentUpdateForm(ModelForm):
+
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(StudentUpdateForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+
+        # set form tag atributes
+        self.helper.form_action = reverse('students_edit',
+                                          kwargs={'pk': kwargs['instance'].id}
+                                         )
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'form_horizontal'
+
+        # set form field properties
+        self.helper.help_text_inline = True
+        self.helper.html5_required = True
+        self.helper.label_class = 'col-sm-2 control-label'
+        self.helper.field_class = 'col-sm-10'
+
+        # add buttons
+        self.helper.layout[-1] = FormActions(
+            Submit('add_button', 'Зберегти', css_class='btn btn-primary'),
+            Submit('cancel_button', 'Скасувати', css_class='btn btn-link')
+        )
+
 
 class StudentsUpdateView(UpdateView):
+
     model = Student
-    fields = ['first_name', 'last_name', 'middle_name', 'student_group',
-              'birthday', 'photo', 'ticket', 'notes']
     template_name = 'students/students_edit.html'
+
+    form_class = StudentUpdateForm
 
     def get_success_url(self):
         return f"{reverse('home')}?status_message=Студента успішно збережено"
