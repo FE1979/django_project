45c430b32cc7c569f063c4ae7300405efe029a9e
FE1979 <ronin911@gmail.com>
2019-08-14 12:55:42 +0300

    Refresh admin view

--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Student
+from django.urls import reverse
+
+from .models import Student, Group
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 'middle_name', 'ticket',
+                     'notes']
+
+    def view_on_site(self, obj):
+        return reverse('students_edit', args=[obj.id])
+
 
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Group)
+
